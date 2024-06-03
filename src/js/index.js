f8a5cc5c7b2e1a298eb64368d10c4b04c7b97f54
guidojosesaban <80138825+guidojosesaban@users.noreply.github.com>
2024-06-03 00:32:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const menuBtn = document.getElementById('menuBtn');
+const menu = document.getElementsByClassName('mainMenu')[0];
+
+menuBtn.addEventListener("click", function(event){
+    event.stopPropagation(); 
+    if(menu.classList.contains('displayYes')){
+        menu.classList.remove('displayYes');
+        menuBtn.setAttribute('src', './assets/icons/menuIcon.svg')
+    } else {
+        menu.classList.add('displayYes');
+        menuBtn.setAttribute('src', './assets/icons/cancel.svg')
+    }
+});
+
+document.addEventListener("click", function(){
+    if(menu.classList.contains('displayYes')){
+        menu.classList.remove('displayYes');
+        menuBtn.setAttribute('src', './assets/icons/menuIcon.svg')
+    }
+});
